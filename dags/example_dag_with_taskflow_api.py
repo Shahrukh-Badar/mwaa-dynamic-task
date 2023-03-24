e2028c13f2323645c0a49cd69b462f9e60f9c8ae
ShahrukhBadar <shahrukh.badar@z-lab.com>
2023-03-24 16:56:54 +0100

    feat: added DAG with mock data

---
+++ dags/example_dag_with_taskflow_api.py
@@ -0,0 +1,28 @@
+import json
+from airflow.decorators import dag, task
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+}
+
+
+@dag(default_args=default_args, schedule_interval="@daily", start_date=days_ago(2),
+     tags=['MWAA', 'DYNAMIC TASKS', 'TASKFLOW API', 'MAP TRANSFORM REDUCE'])
+def dag_taskflow_api_dynamic_tasks():
+    @task()
+    def get_data():
+        return [
+            {"id": 1, "name": "Golden Retriever", "description": "A happy golden retriever playing with a toy",
+             "URL": "https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528_1280.jpg"},
+            {"id": 2, "name": "Siamese Cat", "description": "A beautiful Siamese cat with blue eyes",
+             "URL": "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"},
+            {"id": 3, "name": "Nature", "description": "A scenic view of a mountain range with trees",
+             "URL": "https://cdn.pixabay.com/photo/2016/02/13/13/11/mountains-1199028_1280.jpg"},
+            {"id": 4, "name": "City", "description": "A night view of Tokyo city skyline",
+             "URL": "https://cdn.pixabay.com/photo/2016/08/31/23/22/tokyo-1631703_1280.jpg"}]
+
+    get_data()
+
+
+dag_with_taskflow_api = dag_taskflow_api_dynamic_tasks()
